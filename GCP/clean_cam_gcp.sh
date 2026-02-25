baf253430d511ebb85c33663930f402db50d000d
Yessenia Becerra <yessenia_becerra@trendmicro.com>
2026-02-25 14:08:53 -0300

    Update clean_cam_gcp.sh

--- GCP/clean_cam_gcp.sh
+++ GCP/clean_cam_gcp.sh
@@ -13,19 +13,19 @@ for PROJECT_ID in $PROJECTS; do
     echo ">> Project: $PROJECT_ID"
     gcloud config set project $PROJECT_ID --quiet
 
-    gcloud run services list --filter="metadata.name:$PREFIX" --format="value(metadata.name)" --region=$REGION | xargs -r -I {} gcloud run services delete {} --region=$REGION --quiet
-    gcloud run jobs list --filter="metadata.name:$PREFIX" --format="value(metadata.name)" --region=$REGION | xargs -r -I {} gcloud run jobs delete {} --region=$REGION --quiet
+    #gcloud run services list --filter="metadata.name:$PREFIX" --format="value(metadata.name)" --region=$REGION | xargs -r -I {} gcloud run services delete {} --region=$REGION --quiet
+    #gcloud run jobs list --filter="metadata.name:$PREFIX" --format="value(metadata.name)" --region=$REGION | xargs -r -I {} gcloud run jobs delete {} --region=$REGION --quiet
 
-    gcloud eventarc triggers list --filter="name:$PREFIX" --format="value(name)" --location=$REGION | xargs -r -I {} gcloud eventarc triggers delete {} --location=$REGION --quiet
-    gcloud workflows delete "${PREFIX}-disk-scan-workflow" --location=$REGION --quiet 2>/dev/null
+    #gcloud eventarc triggers list --filter="name:$PREFIX" --format="value(name)" --location=$REGION | xargs -r -I {} gcloud eventarc triggers delete {} --location=$REGION --quiet
+    #gcloud workflows delete "${PREFIX}-disk-scan-workflow" --location=$REGION --quiet 2>/dev/null
 
-    gcloud pubsub subscriptions list --filter="name:$PREFIX" --format="value(name)" | xargs -r -I {} gcloud pubsub subscriptions delete {} --quiet
-    gcloud pubsub topics list --filter="name:$PREFIX" --format="value(name)" | xargs -r -I {} gcloud pubsub topics delete {} --quiet
+    #gcloud pubsub subscriptions list --filter="name:$PREFIX" --format="value(name)" | xargs -r -I {} gcloud pubsub subscriptions delete {} --quiet
+    #gcloud pubsub topics list --filter="name:$PREFIX" --format="value(name)" | xargs -r -I {} gcloud pubsub topics delete {} --quiet
 
-    gsutil ls | grep "$PREFIX" | xargs -r -I {} gsutil rm -r {}
-    gcloud secrets list --filter="name:$PREFIX" --format="value(name)" | xargs -r -I {} gcloud secrets delete {} --quiet
+    #gsutil ls | grep "$PREFIX" | xargs -r -I {} gsutil rm -r {}
+    #gcloud secrets list --filter="name:$PREFIX" --format="value(name)" | xargs -r -I {} gcloud secrets delete {} --quiet
 
-    gcloud logging sinks list --filter="name:$PREFIX" --format="value(name)" | xargs -r -I {} gcloud logging sinks delete {} --quiet
+    #gcloud logging sinks list --filter="name:$PREFIX" --format="value(name)" | xargs -r -I {} gcloud logging sinks delete {} --quiet
     gcloud iam roles list --project=$PROJECT_ID --format="value(name)" | grep -E "v1-avtd|Vision_One_CAM" | xargs -r -I {} gcloud iam roles delete {} --project=$PROJECT_ID --quiet
     gcloud iam service-accounts list --filter="email:$PREFIX" --format="value(email)" | xargs -r -I {} gcloud iam service-accounts delete {} --quiet
 
